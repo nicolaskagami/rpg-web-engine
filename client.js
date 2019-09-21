c3bc9b6caa91c5a09a7b5d746bdf84d63fc7fd5e
Nicolas Kagami <nicolaskagami@gmail.com>
2019-09-21 19:01:35 -0300

    Client Communication and cleanup

--- client.js
+++ client.js
@@ -6,7 +6,18 @@ var socket = io.connect('http://localhost:3000', {reconnect: true});
 socket.on('connect', ()=> {
     console.log('Connected!');
     socket.emit('add user', "123");
+    socket.emit("new message", "123")
 });
-    socket.on("new message", (data) => {
-        console.log(data.username+": "+data.message)});
+socket.on("new message", (data) => {
+    console.log(data.username+": "+data.message)});
 
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', function(line) {
+    socket.emit("new message", line)
+    rl.prompt();
+}).on('close',function(){
+    process.exit(0);
+});
