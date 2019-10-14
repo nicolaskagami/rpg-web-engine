56eae771fb45ec8bb54121917e63169731cb460a
nicolaskagami <nicolaskagami@gmail.com>
2019-10-14 13:49:39 -0300

    Expression revamp: removed JSON copy, new recursive InternalScope

--- js/entities/expression.js
+++ js/entities/expression.js
@@ -34,47 +34,67 @@ class Expression extends Entity
                 if(ast.arguments[arg].property && ast.arguments[arg].property.name === 'output')
                     if(this.__manager.entities[ast.arguments[arg].object.name])
                         this.__manager.entities[ast.arguments[arg].object.name].evaluate()
-        var scope = JSON.parse(this.__manager.getJSONEntitiesMap());
-        var internalScope = JSON.parse(this.__manager.getJSONEntitiesMap());
-        scope.FILTER= function(array,expression)
-        {
-            var result = []
-            for(var i=0; i < array.length;i++) 
-            {
-                var ent = '';
-                var ast2 = expr.parse(expression)
-                if(internalScope[array[i]])
-                    ent = internalScope[array[i]];
-                for (const key of Object.keys(ent)) 
-                    internalScope[key] = ent[key];
-                if(expr.eval(ast2, internalScope))
-                    result.push(array[i])
-            }
-            return result;
-        }
-                    
-        scope.OR = function(a,b)
-        {
-            var c = a.concat(b)
-            for(var i=0; i<c.length; i++) 
-                for(var j=i+1; j<c.length; j++)
-                    if(c[i] === c[j])
-                        c.splice(j--, 1);
-            return c;
-        }
-        scope.AND = function(a,b)
-        {
-            var c = [] 
-            for(var i=0; i<a.length; i++) 
-                for(var j=0; j<b.length; j++)
-                    if(a[i] === b[j])
-                        c.push(a[i])
-            return c;
-        }
+        var scope = new Scope(this.__manager.getEntities());
         try {
             this.output = (expr.eval(this.ast, scope))
             return this.output;
         } catch (error) { return this.defaultValue; }
+    }
+}
+var __entities ={};
+class Scope
+{
+    constructor(entities)
+    {
+        __entities = entities;
+        for(const key of Object.keys(entities))
+        {
+            this[key] = entities[key]
+        }
+        
+    }
+    static newInternalScope(ent)
+    {
+        if(__entities[ent]) {
+            var internalScope = new Scope(__entities) 
+            for (const key of Object.keys(__entities[ent])) 
+                internalScope[key] = __entities[ent][key];
+            return internalScope;
+            console.log(internalScope)
+        } else {
+            return null;    
+        }
+    }
+    FILTER(array,expression)
+    {
+        var result = []
+        for(var i=0; i < array.length;i++) 
+        {
+            var obj= '';
+            var ast2 = expr.parse(expression)
+            var internalScope = Scope.newInternalScope(array[i]);
+            if((internalScope != null) && (expr.eval(ast2, internalScope)))
+                    result.push(array[i])
+        }
+        return result;
+    }
+    OR(a,b)
+    {
+        var c = a.concat(b)
+        for(var i=0; i<c.length; i++) 
+            for(var j=i+1; j<c.length; j++)
+                if(c[i] === c[j])
+                    c.splice(j--, 1);
+        return c;
+    }
+    AND(a,b)
+    {
+        var c = [] 
+        for(var i=0; i<a.length; i++) 
+            for(var j=0; j<b.length; j++)
+                if(a[i] === b[j])
+                    c.push(a[i])
+        return c;
     }
 }
 module.exports = Expression;
---
+++ other/test.js
@@ -0,0 +1,27 @@
+class A 
+{
+    constructor()
+    {
+        this.baseSTR = 2
+    }
+}
+var a = new A
+function defGetSet (object,field_name) {
+    Object.defineProperty (object, field_name, {
+        get: function () { 
+            console.log('GET', field_name);
+            return this.baseSTR + 1;
+        },
+        set: function (new_value) {
+            console.log('NO SET FOR YOU', field_name, new_value);
+        }
+    });
+}
+defGetSet(a,'str');
+a.str=2
+console.log(a.str)
+console.log("Stringified")
+a2 = JSON.parse(JSON.stringify(a))
+console.log(a2.str)
+a2.str=2
+console.log(a2.str)
