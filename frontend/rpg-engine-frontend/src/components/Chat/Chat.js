ade9e9679ddae0e84c337cd39a0206e967a6db8f
Gabriel Thier <gabriel.thier@gmail.com>
2020-06-18 00:21:23 -0300

    Adjusted chat input and chat log

--- frontend/rpg-engine-frontend/src/components/Chat/Chat.js
+++ frontend/rpg-engine-frontend/src/components/Chat/Chat.js
@@ -23,10 +23,34 @@ class Chat extends Component {
                 tag: "combat",
                 message: "hello world 3",
                 timestamp: "10:43"
-            }
+            },
+            {
+                source: "user1", // physics, magic, gab
+                tag: "combat",
+                message: "hello world",
+                timestamp: "10:30"
+            },
+            {
+                source: "user1", // physics, magic, gab
+                tag: "combat",
+                message: "hello world",
+                timestamp: "10:30"
+            },
+            {
+                source: "user1", // physics, magic, gab
+                tag: "combat",
+                message: "hello world",
+                timestamp: "10:30"
+            },
         ];
-        return <div className="Chat" style={{ width: "100%", "overflow-y": "scroll", height: "100%"}}>
-           {messages.map((message, index) => <ChatMessage key={index} message={message}> </ChatMessage>)}
+
+        // Needs fixed position on .ChatInput, CSS doesn't work properly
+        // Needs height:% on .chat-log instead of fixed height
+
+        return <div className="Chat" style={{ width: "100%", height: "100%"}}> 
+           <div className="chat-log"> 
+               {messages.map((message, index) => <ChatMessage key={index} message={message}> </ChatMessage>)}
+           </div>
             <ChatInput></ChatInput>
         </div>
     };
